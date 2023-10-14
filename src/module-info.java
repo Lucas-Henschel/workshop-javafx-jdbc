d72662e670e879aeac98be0f0a5782cb0c886f0c
Lucas Gabriel Henschel <lucas101henschel@gmail.com>
2023-10-14 15:52:01 -0300

    Department Service

--- src/module-info.java
+++ src/module-info.java
@@ -5,5 +5,8 @@ module workshop_javafx {
 	exports gui;
 	opens gui to javafx.fxml;
 	
+    opens model.entities to javafx.graphics, javafx.fxml, javafx.base;
+    opens model.services to javafx.graphics, javafx.fxml;
+	
 	opens application to javafx.graphics, javafx.fxml;
 }
