10efc78b1d8dbfef8779ac35013df2ebc1374495
Lucas Gabriel Henschel <lucas101henschel@gmail.com>
2023-10-14 14:46:47 -0300

    DepartmentList view design

--- src/gui/MainVIewController.java
+++ src/gui/MainVIewController.java
@@ -32,7 +32,7 @@ public class MainVIewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		System.out.println("menuItemDepartment");
+		loadView("/gui/DepartmentList.fxml");
 	}
 	
 	@FXML
